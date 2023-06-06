d46caaa2aa479dcf4be89c2ed83087735b0fe1b4
user <Klimovich.maks@gmail.com>
2023-06-06 12:10:57 +0300

    add sem2 task's

---
+++ task4.java
@@ -0,0 +1,37 @@
+package Sminar2;
+
+import java.io.*;
+
+/**
+ * Заставьте выполниться следующий код
+ */
+public class task4 {
+    public static void main(String[] args) {
+        InputStream inputStream;
+        try {
+            String[] strings = {"asdf", "asdf"};
+            String str = strings[1];
+            int a = 1 / 1;
+            test();
+
+            inputStream = new FileInputStream("sdafgdsaf");
+        } catch (ArithmeticException e) {
+            System.err.println("Делить на ноль нельзя");
+        } catch (StackOverflowError error) {
+            System.err.println("stack overflow error");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        } finally {
+            System.out.println("Я все равно выполнился!");
+        }
+        System.out.println("Я жив!");
+    }
+
+    private static void test() throws IOException {
+        File file = new File("1");
+        file.createNewFile();
+        FileReader reader = new FileReader(file);
+        reader.read();
+        test();
+    }
+}
